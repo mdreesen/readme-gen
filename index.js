e81ad4ecb5075bcd387051e5d40d70cb8fd08602
mdreesen <56687081+mdreesen@users.noreply.github.com>
2020-09-19 22:43:22 -0600

    changed up the index code to have one questions array and changed up the callback of the questions array to be simpler. This creates less clutter and works

--- index.js
+++ index.js
@@ -9,102 +9,67 @@ const { writeFile, copyFile } = require('./utils/generate-readme.js')
 const projectName = () => {
     console.log(
         `
-        ============
-        Project Name
-        ============
+        =====================
+        Lets Make Your ReadMe
+        =====================
         `
-    )
-    return inquirer.prompt([{
-        type: 'input',
-        name: 'project-name',
-        message: 'Please enter the name of your project. (Required)',
-        validate: ProjectNameInput => {
-            if (ProjectNameInput) {
-                return true;
-            } else {
-                console.log('Please enter the name of your project.');
-                return false;
-            }
-        }
-    }])
-};
-
-const projectDetails = () => {
-    console.log(
-        `
-        ===============
-        Project Details
-        ===============
-        `
-    )
-    return inquirer
-        .prompt([{
-                type: 'input',
-                name: 'description',
-                message: 'What is the description of your project? (Required)',
-                validate: descriptionInput => {
-                    if (descriptionInput) {
-                        return true;
-                    } else {
-                        console.log('What is the description of your project?')
-                        return false;
-                    }
-                }
-            },
-            {
-                input: 'input',
-                name: 'installation',
-                message: 'What is the command to install your project?',
-                default: 'npm i'
-            },
-            {
-                type: 'input',
-                name: 'usage',
-                message: 'What does the user need to know about using this repo?'
-            },
-            {
-                type: 'input',
-                name: 'contribution',
-                message: 'What does the user need to know about contributing to this repo?'
-            },
-            {
-                type: 'input',
-                name: 'tests',
-                message: 'What command should be used to run tests for this project?',
-                default: 'npm test'
-            }
-        ])
-};
-
-
-
-const licensePrompt = () => {
-    // If there is no license array, create one!
-    console.log(
-        `
-            =============
-            Add A License
-            =============
-            `
-    );
-    return inquirer.prompt([{
-        type: 'list',
-        name: 'license',
-        choices: ['Apache', 'GNU', 'NPM', 'BSD', 'Rust']
-    }])
-};
-
-const userInfo = () => {
-    console.log(
-        `
-            =========
-            User Info
-            =========
-            `
     )
     return inquirer.prompt([{
             type: 'input',
-            name: 'Github-userName',
+            name: 'projectName',
+            message: 'Please enter the name of your project. (Required)',
+            validate: ProjectNameInput => {
+                if (ProjectNameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the name of your project.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'What is the description of your project? (Required)',
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                    return true;
+                } else {
+                    console.log('What is the description of your project?')
+                    return false;
+                }
+            }
+        },
+        {
+            input: 'input',
+            name: 'installation',
+            message: 'What is the command to install your project?',
+            default: 'npm i'
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'What does the user need to know about using this repo?'
+        },
+        {
+            type: 'input',
+            name: 'contribution',
+            message: 'What does the user need to know about contributing to this repo?'
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'What command should be used to run tests for this project?',
+            default: 'npm test'
+        },
+        {
+            type: 'list',
+            name: 'license',
+            choices: ['Apache', 'GNU', 'NPM', 'BSD', 'Rust']
+        },
+        {
+            type: 'input',
+            name: 'github',
             message: 'Please enter your github username. (Required)',
             validate: githubName => {
                 if (githubName) {
@@ -128,27 +93,17 @@ const userInfo = () => {
         },
         {
             type: 'input',
-            name: 'questions for me',
+            name: 'questionsForMe',
             message: 'You can reach me via email at "mdreesen90@gmail.com" with your question(s), thank you!',
             when: ({ questions }) => questions
         }
-
     ])
 };
 
 
 projectName()
-    .then(projectDetails)
-    .then(projectDetails => {
-        return generateReadme(projectDetails);
-    })
-    .then(licensePrompt)
-    .then(licensePrompt => {
-        return generateReadme(licensePrompt);
-    })
-    .then(userInfo)
-    .then(userInfo => {
-        return generateReadme(userInfo)
+    .then(projectName => {
+        return generateReadme(projectName)
     })
     .then(pageReadme => {
         return writeFile(pageReadme)
--- readme-template.js
+++ readme-template.js
@@ -1,5 +1,38 @@
 const generateReadme = data => {
     return `
+    # ${data.projectName}
+
+    # Description
+    ${data.description}
+
+    # Installation
+    ${data.installation}
+
+    ${data.usage}
+
+    ${data.contribution}
+
+    ${data.tests}
+
+    ${data.license}
+
+    # User Info
+
+    ## Github user
+    ${data.github}
+
+    ## Email
+    ${data.email}
+
+    ${data.questions}
+
+    ${data.questionsForMe}
+    `
+};
+
+/*
+const generateReadme = data => {
+    return `
     # ${data.project-name}
 
     ${data.description}
@@ -19,5 +52,6 @@ const generateReadme = data => {
     ${data.email}
     `
 };
+*/
 
 module.exports = generateReadme;
